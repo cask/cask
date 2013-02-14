b0a1d94fe00546d6dc17f0864b5d82bc67da579e
Johan Andersson <johan.rejeep@gmail.com>
2013-02-14 18:54:26 +0100

    Update to version 0.1.1.

--- carton.el
+++ carton.el
@@ -5,7 +5,7 @@
 
 ;; Author: Johan Andersson <johan.rejeep@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Keywords: speed, convenience
 ;; URL: http://github.com/rejeep/carton
 
