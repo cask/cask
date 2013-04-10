c0758878a9452e4699bd0cb67bf8e941e5af40a1
Johan Andersson <johan.rejeep@gmail.com>
2013-04-10 18:53:42 +0200

    Update to version v0.2.0.

--- carton-pkg.el
+++ carton-pkg.el
@@ -1,3 +1,2 @@
-(define-package "Carton" "0.1.2"
-  "Emacs dependency management made easy"
-  '((s "1.3.1")))
+(define-package "Carton" "0.2.0"
+  "Emacs dependency management made easy")
--- carton.el
+++ carton.el
@@ -5,7 +5,7 @@
 
 ;; Author: Johan Andersson <johan.rejeep@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
-;; Version: 0.1.2
+;; Version: 0.2.0
 ;; Keywords: speed, convenience
 ;; URL: http://github.com/rejeep/carton
 
