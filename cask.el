6e64c550c32473d32840395b3b475edca7c6ccf9
Johan Andersson <johan.rejeep@gmail.com>
2013-07-29 22:49:49 +0200

    Force users to rerun cask.
    Previously, the old `elpa` directory was used if it existed. This is
    bad for many reasons, one is because we now support multiple versions
    of Emacs.

--- cask.el
+++ cask.el
@@ -154,16 +154,8 @@ SCOPE may be nil or :development."
 
 (defun cask-elpa-dir ()
   "Return full path to `cask-project-path'/.cask/elpa/`emacs-version'."
-  (let ((carton-elpa-dir
-         (expand-file-name "elpa" cask-project-path))
-        (cask-elpa-dir
-         (expand-file-name (format ".cask/%s/elpa" emacs-version) cask-project-path)))
-    (if (and (file-exists-p carton-elpa-dir)
-             (not (file-exists-p cask-elpa-dir)))
-        (progn
-          (message "[DEPRECATION WARNING] Remove 'elpa' directory and run 'cask' command again")
-          carton-elpa-dir)
-      cask-elpa-dir)))
+  (expand-file-name (format ".cask/%s/elpa" emacs-version)
+                    cask-project-path))
 
 (defun cask-setup (project-path)
   "Setup cask for project at PROJECT-PATH."
