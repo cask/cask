c56a7cba004c86992f3b155eabc628d9f22b9ced
Johan Andersson <johan.rejeep@gmail.com>
2013-08-13 11:51:28 +0200

    Release v0.4.5.

--- cask.el
+++ cask.el
@@ -6,7 +6,7 @@
 
 ;; Author: Johan Andersson <johan.rejeep@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
-;; Version: 0.4.4
+;; Version: 0.4.5
 ;; Keywords: speed, convenience
 ;; URL: http://github.com/rejeep/cask.el
 
