223bd70b47216b48d0bee565222ed75467070bfc
Johan Andersson <johan.rejeep@gmail.com>
2013-12-26 13:06:51 +0100

    We can use f in the tests if it's unloaded.

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,20 +1,29 @@
-;; NOTE:
-;; Do not require any helper library such as f, s or dash in
-;; here, because then they will pollute the test enviroment by making
-;; those libraries available when cask.el run as a test, but not when
-;; used as intended.
-;;
-;; There is another issue since ert-runner is executed with `cask
-;; exec`, which means that all Cask dependencies will be in
-;; `load-path' here. All of those are removed below to get a clean
-;; environment.
+(require 'f)
 
 (defvar cask-test/test-path
-  (file-name-directory load-file-name))
+  (f-parent (f-this-file)))
 
 (defvar cask-test/root-path
-  (file-name-directory (directory-file-name cask-test/test-path)))
+  (f-parent cask-test/test-path))
 
+(defvar cask-test/sandbox-path
+  (f-expand "sandbox" cask-test/test-path))
+
+(defvar cask-test/package-path
+  (f-expand "package" cask-test/sandbox-path))
+
+(defvar cask-test/config-path
+  (f-expand "config" cask-test/sandbox-path))
+
+(defmacro with-sandbox (&rest body)
+  `(let ((default-directory cask-test/sandbox-path))
+     ,@body))
+
+;; Do not pollute the Cask environment.
+(unload-feature 'f 'force)
+
+;; Since ert-runner is executed with `cask exec` all Cask dependencies
+;; will be in `load-path'. This cleans up the environment.
 (let (clean-load-path)
   (dolist (path load-path)
     (unless (string-prefix-p cask-test/root-path path)
