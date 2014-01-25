76cbf17ac0e1d3927ebecaca1864000731e9bbb5
Johan Andersson <johan.rejeep@gmail.com>
2014-01-25 13:58:17 +0100

    Update dependencies.

--- cask.el
+++ cask.el
@@ -9,7 +9,7 @@
 ;; Version: 0.5.1
 ;; Keywords: speed, convenience
 ;; URL: http://github.com/cask/cask
-;; Package-Requires: ((s "1.8.0") (dash "2.2.0") (f "0.10.0") (epl "0.0.1") (cl-lib "0.3"))
+;; Package-Requires: ((s "1.8.0") (dash "2.2.0") (f "0.16.0") (epl "0.5") (shut-up "0.1.0") (cl-lib "0.3") (package-build "0.1"))
 
 ;; This file is NOT part of GNU Emacs.
 
