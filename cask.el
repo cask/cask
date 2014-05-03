298289c53be845ee89d0518ec17c43469837be48
Johan Andersson <johan.rejeep@gmail.com>
2014-05-03 10:55:25 +0200

    Use variable instead of hardcoded value.

--- cask.el
+++ cask.el
@@ -747,7 +747,7 @@ If no such dependency exist, return nil."
 
 (defun cask-file (bundle)
   "Return path to BUNDLE Cask-file."
-  (f-expand "Cask" (cask-path bundle)))
+  (f-expand cask-filename (cask-path bundle)))
 
 (defun cask-files (bundle)
   "Return BUNDLE files list.
