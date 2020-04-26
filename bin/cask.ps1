decaac32364f0b0f0b509e23a1cf202646cf47cc
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2020-04-26 13:14:12 +0700

    Add support for 'emacs' and 'exec' command to cask.ps1

--- bin/cask.ps1
+++ bin/cask.ps1
@@ -1,1 +1,30 @@
-emacs -Q --batch --script "$env:UserProfile\.cask\cask-cli.el" -- $args
+function Exec-Cask {
+    emacs -Q --batch --script "$Env:UserProfile\.cask\cask-cli.el" -- @args
+}
+
+function Exec-Command($cmd) {
+    $emacsLoadPath = $Env:EMACSLOADPATH
+    $path = $Env:Path
+    try {
+        $Env:EMACSLOADPATH = "$(Exec-Cask load-path)"
+        $Env:Path = "$(Exec-Cask path)"
+        & $cmd @args
+   } finally {
+        $Env:EMACSLOADPATH = $emacsLoadPath
+        $Env:Path = $path
+    }
+}
+
+function Exec-Emacs {
+    Exec-Command emacs @args
+}
+
+$caskCmd = $args[0]
+
+if ($caskCmd -eq "emacs") {
+    Exec-Emacs $args[1..($args.Count - 1)]
+} elseif ($caskCmd -eq "exec") {
+    Exec-Command $args[1..($args.Count - 1)]
+} else {
+    Exec-Cask $args
+}
