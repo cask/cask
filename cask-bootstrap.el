57437882b1a7c492b6d082f11d9012366f7347e9
Naoya Yamashita <conao3@gmail.com>
2021-02-09 08:00:34 +0900

    clean code

--- cask-bootstrap.el
+++ cask-bootstrap.el
@@ -30,9 +30,7 @@
 (defvar cask-directory)
 
 (defconst cask-bootstrap-emacs-version
-  (format "%s.%s"
-          emacs-major-version
-          emacs-minor-version))
+  (format "%s.%s" emacs-major-version emacs-minor-version))
 
 (defconst cask-bootstrap-dir
   (expand-file-name
