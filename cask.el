0330c8b3e9f0e5969c7e904ddad6fcbc1855e499
dickmao <none>
2021-04-11 06:26:18 -0400

    bump version

--- cask.el
+++ cask.el
@@ -6,10 +6,10 @@
 
 ;; Author: Johan Andersson <johan.rejeep@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
-;; Version: 0.8.7
+;; Version: 0.8.8-pre
 ;; Keywords: speed, convenience
 ;; URL: http://github.com/cask/cask
-;; Package-Requires: ((emacs "24.1") (s "1.8.0") (f "0.16.0") (epl "0.5") (shut-up "0.1.0") (cl-lib "0.3") (package-build "1.2") (ansi "0.4.1"))
+;; Package-Requires: ((emacs "24.1") (s "1.8.0") (f "0.16.0") (epl "0.5") (shut-up "0.1.0") (cl-lib "0.3") (package-build "0") (ansi "0.4.1"))
 
 ;; This file is NOT part of GNU Emacs.
 
